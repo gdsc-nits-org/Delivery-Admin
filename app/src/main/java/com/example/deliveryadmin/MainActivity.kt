0bec3e579a788cef9ed513faf726ae443f0062be
Gaurav-822 <gaurav.moocs@gmail.com>
2024-05-26 20:56:57 +0530

    Bottom Navbar Added

--- app/src/main/java/com/example/deliveryadmin/MainActivity.kt
+++ app/src/main/java/com/example/deliveryadmin/MainActivity.kt
@@ -2,35 +2,51 @@ package com.example.deliveryadmin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.deliveryadmin.HomePageFragments.HomeFragment
+import com.example.deliveryadmin.HomePageFragments.MenuFragment
+import com.example.deliveryadmin.HomePageFragments.ProfileFragment
+import com.example.deliveryadmin.HomePageFragments.SearchFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bottomNavigationView: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val navView: BottomNavigationView = findViewById(R.id.bottom_navigation)
-        navView.setOnItemSelectedListener { menuItem ->
+        bottomNavigationView = findViewById(R.id.bottom_navigation)
+        bottomNavigationView.setOnItemSelectedListener { menuItem ->
             when (menuItem.itemId) {
                 R.id.navigation_home -> {
-                    // Navigate to Home Fragment
+                    // Replace fragment with HomeFragment
+                    replaceFragment(HomeFragment())
                     true
                 }
                 R.id.navigation_search -> {
-                    // Navigate to Dashboard Fragment
+                    // Replace fragment with CartFragment
+                    replaceFragment(SearchFragment())
                     true
                 }
                 R.id.navigation_menu -> {
-                    // Navigate to Notifications Fragment
+                    // Replace fragment with SearchFragment
+                    replaceFragment(MenuFragment())
                     true
                 }
                 R.id.navigation_profile -> {
-                    // Navigate to Notifications Fragment
+                    // Replace fragment with ProfileFragment
+                    replaceFragment(ProfileFragment())
                     true
                 }
                 else -> false
             }
         }
+        replaceFragment(HomeFragment())
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().replace(R.id.frame_container, fragment).commit()
     }
 }
