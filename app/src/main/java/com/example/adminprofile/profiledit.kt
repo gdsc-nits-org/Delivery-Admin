888d53d3b4d5e15dff973b3a15c03bea00fc8a47
devmeh19 <devmehrotra@gmail.com>
2024-06-13 19:10:30 +0530

    Admin Profile

---
+++ app/src/main/java/com/example/adminprofile/profiledit.kt
@@ -0,0 +1,11 @@
+package com.example.adminprofile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class profiledit : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profiledit)
+    }
+}
