01054e894767f0ab20c273ca9ad0fbf94d902a79
Rishi Gorai <franistine384@gmail.com>
2024-06-27 21:49:18 +0530

    Improvements in UI + Forgot password

--- app/src/main/java/com/example/deliveryadmin/Fragments/SignUp.kt
+++ app/src/main/java/com/example/deliveryadmin/Fragments/SignUp.kt
@@ -1,101 +1,107 @@
 package com.example.deliveryadmin.Fragments
 
 import android.os.Bundle
+import android.util.Patterns
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.activity.OnBackPressedCallback
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import com.example.deliveryadmin.R
-import com.example.deliveryadmin.Utils.Client
 import com.example.deliveryadmin.databinding.FragmentSignUpBinding
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 
 class SignUp : Fragment() {
 
-    private lateinit var auth: FirebaseAuth
     private lateinit var binding: FragmentSignUpBinding
     private lateinit var navController: NavController
-    private lateinit var databaseRef: DatabaseReference
+    private lateinit var auth: FirebaseAuth
+    private lateinit var database: FirebaseDatabase
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        navController = Navigation.findNavController(view)
+
+        binding.backBtnSignUpPage.setOnClickListener {
+            activity?.supportFragmentManager?.popBackStack();
+        }
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        binding= FragmentSignUpBinding.inflate(inflater,container,false)
+        binding = FragmentSignUpBinding.inflate(inflater, container, false)
+
+        binding.btnSignUp.setOnClickListener {
+            if (checkAllFields()) {
+                auth = FirebaseAuth.getInstance()
+                database = FirebaseDatabase.getInstance()
+
+                val email = binding.etEmailSignUpPage.text.toString().trim()
+                val password = binding.etPassSignUpPage.text.toString().trim()
+
+                signUp(email, password)
+            }
+        }
+
+        binding.tvLoginPage.setOnClickListener {
+            navController.navigate(R.id.action_signUpPage_to_loginPage)
+        }
+
         return binding.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    private fun signUp(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(
+            OnCompleteListener {
+                if (it.isSuccessful) {
+                    Toast.makeText(context, "Login Successfully", Toast.LENGTH_SHORT).show()
+                    navController.navigate(R.id.action_loginPage_to_locationFragment)
 
-        init(view)
-        registerEvents()
-
-    }
-
-    private fun init(view: View) {
-        navController= Navigation.findNavController(view)
-        auth=FirebaseAuth.getInstance()
-        databaseRef = FirebaseDatabase.getInstance()
-            .reference.child("Clients")
-    }
-    private fun registerEvents() {
-        binding.backBtnSignUpPage.setOnClickListener {
-            navController.navigate(R.id.action_signUpPage_to_signIn)
-        }
-
-        binding.btnSignUp.setOnClickListener {
-            val name = binding.etNameSignUpPage.text.toString().trim()
-            val email = binding.etEmailSignUpPage.text.toString().trim()
-            val pass = binding.etPassSignUpPage.text.toString().trim()
-            val repass = binding.etRePassSignUpPage.text.toString().trim()
-
-            if (name.isNotEmpty() && email.isNotEmpty() && pass.isNotEmpty() && repass.isNotEmpty() ) {
-                if (pass == repass) {
-
-                    auth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener { authTask ->
-                        if (authTask.isSuccessful) {
-                            val currentUser = auth.currentUser
-                            val userId = currentUser?.uid
-
-                            val clientModel = Client(name, email)
-                            userId?.let {
-                                databaseRef.child(it).setValue(clientModel)
-                                    .addOnCompleteListener { dbTask ->
-                                        if (dbTask.isSuccessful) {
-                                            Toast.makeText(context, "Registered Successfully", Toast.LENGTH_SHORT).show()
-                                            navController.navigate(R.id.action_signUpPage_to_locationFragment)
-                                        } else {
-                                            Toast.makeText(context, "User could not be added", Toast.LENGTH_SHORT).show()
-                                        }
-                                    }
-                            }
-                        } else {
-                            Toast.makeText(context, authTask.exception?.message, Toast.LENGTH_SHORT).show()
-                        }
-                    }
                 } else {
-                    Toast.makeText(context, "Passwords do not match", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(
+                        context,
+                        "Your account does not exist!\n SignUp first!!",
+                        Toast.LENGTH_SHORT
+                    ).show()
                 }
-            } else {
-                Toast.makeText(context, "Please fill up all the necessary details", Toast.LENGTH_SHORT).show()
             }
+        )
+    }
+
+    private fun checkAllFields(): Boolean {
+        val email = binding.etEmailSignUpPage.text.toString()
+        if (email == "") {
+            binding.etEmailSignUpPage.error = "This is required field"
+            return false
         }
 
-        val callback = object : OnBackPressedCallback(true) {
-            override fun handleOnBackPressed() {
-                // Log.d("TAG", "Pressed...")
-            }
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            binding.etEmailSignUpPage.error = "Please enter a valid Email"
+            return false
         }
 
-        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, callback)
+        if (binding.etPassSignUpPage.text.toString() == "") {
+            binding.etPassSignUpPage.error = "This is required field"
+            return false
+        }
+
+        if (binding.etRePassSignUpPage.text.toString() == "") {
+            binding.etRePassSignUpPage.error = "This is required field"
+            return false
+        }
+
+        if (binding.etPassSignUpPage.text.toString() != binding.etRePassSignUpPage.text.toString()) {
+            binding.etPassSignUpPage.error = "Password do not match"
+            return false
+        }
+        return true
     }
 
 }
