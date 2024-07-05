af23c93eb0b3128e482ad28839f0a8a66c67627f
AnKiTk203 <kumar0ankit2003@gmail.com>
2024-07-05 09:25:19 +0530

    Hello

--- app/src/main/java/com/example/deliveryadmin/Fragments/SignUp.kt
+++ app/src/main/java/com/example/deliveryadmin/Fragments/SignUp.kt
@@ -2,16 +2,15 @@ package com.example.deliveryadmin.Fragments
 
 import android.os.Bundle
 import android.util.Patterns
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import com.example.deliveryadmin.R
 import com.example.deliveryadmin.databinding.FragmentSignUpBinding
-import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
 
@@ -27,14 +26,14 @@ class SignUp : Fragment() {
         navController = Navigation.findNavController(view)
 
         binding.backBtnSignUpPage.setOnClickListener {
-            activity?.supportFragmentManager?.popBackStack();
+            activity?.supportFragmentManager?.popBackStack()
         }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
         binding = FragmentSignUpBinding.inflate(inflater, container, false)
 
@@ -58,21 +57,19 @@ class SignUp : Fragment() {
     }
 
     private fun signUp(email: String, password: String) {
-        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(
-            OnCompleteListener {
-                if (it.isSuccessful) {
-                    Toast.makeText(context, "Login Successfully", Toast.LENGTH_SHORT).show()
-                    navController.navigate(R.id.action_loginPage_to_locationFragment)
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener {
+            if (it.isSuccessful) {
+                Toast.makeText(context, "Login Successfully", Toast.LENGTH_SHORT).show()
+                navController.navigate(R.id.action_signUpPage_to_locationFragment)
 
-                } else {
-                    Toast.makeText(
-                        context,
-                        "Your account does not exist!\n SignUp first!!",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }
+            } else {
+                Toast.makeText(
+                    context,
+                    "Your account does not exist!\n SignUp first!!",
+                    Toast.LENGTH_SHORT
+                ).show()
             }
-        )
+        }
     }
 
     private fun checkAllFields(): Boolean {
--- app/src/main/java/com/example/deliveryadmin/HomePageFragments/SearchFragment.kt
+++ app/src/main/java/com/example/deliveryadmin/HomePageFragments/SearchFragment.kt
@@ -7,7 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.deliveryadmin.R
 
-
 class SearchFragment : Fragment() {
 
     override fun onCreateView(
